e23ae0399fe3a4c925af577b8ed37873e3fd81bd
letsbehuman <letsbehumandesign@gmail.com>
2023-01-22 16:15:51 +0100

    adding get-post controller

--- src/shared/services/db/post.services.ts
+++ src/shared/services/db/post.services.ts
@@ -1,4 +1,4 @@
-import { IPostDocument } from '@post/interfaces/post.interface';
+import { IGetPostsQuery, IPostDocument } from '@post/interfaces/post.interface';
 import { PostModel } from '@post/models/post.schema';
 import { IUserDocument } from '@user/interfaces/user.interface';
 import { UserModel } from '@user/models/user.schema';
@@ -9,6 +9,34 @@ class PostService {
     const post: Promise<IPostDocument> = PostModel.create(createdPost);
     const user: UpdateQuery<IUserDocument> = UserModel.updateOne({ _id: userId }, { $inc: { postsCount: 1 } }); // incremente by 1 in mongoDB
     await Promise.all([post, user]);
+  }
+
+  // <string,1|-1> means the key of Record most be a string and the value 1 or -1
+  public async getPosts(
+    query: IGetPostsQuery,
+    skip = 0,
+    limit = 0,
+    sort: Record<string, 1 | -1>
+  ): Promise<IPostDocument[]> {
+    let postQuery = {};
+    if (query?.imgId && query?.gifUrl) {
+      // $ne = not equal to, operator
+      postQuery = { $or: [{ imgId: { $ne: '' } }, { gifUrl: { $ne: '' } }] };
+    } else {
+      postQuery = query;
+    }
+    const posts: IPostDocument[] = await PostModel.aggregate([
+      { $match: postQuery },
+      { $sort: sort },
+      { $skip: skip },
+      { $limit: limit }
+    ]);
+    return posts;
+  }
+
+  public async postCount(): Promise<number> {
+    const count: number = await PostModel.find({}).countDocuments(); //Counting all documents inside collection
+    return count;
   }
 }
 
